3d322ef7ec12e4940ffae4c02c4b05037b84bd34
parksungjoon0805 <jjoon9908@naver.com>
2024-07-08 16:21:27 +0900

    User->CustomUser

--- apps/contents/models.py
+++ apps/contents/models.py
@@ -1,18 +1,25 @@
+from datetime import timezone
 from django.db import models
-from apps.accounts.models import User
+from apps.accounts.models import CustomUser, Team
 
 class Post(models.Model):
-    id = models.IntegerField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='posts')
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='posts')
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def save(self, *args, **kwargs):
+        self.updated_at = timezone.now()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
 class Image(models.Model):
-    id = models.IntegerField(primary_key=True)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='images')
     image_url = models.ImageField(upload_to='post_images/')
 
-# Create your models here.
+    def __str__(self):
+        return f"Image for post: {self.post.title}"
