3c44981eb79a0709599854128a25d5c00bfcea93
parksungjoon0805 <jjoon9908@naver.com>
2024-07-08 14:03:21 +0900

    content model

--- apps/contents/models.py
+++ apps/contents/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from apps.accounts.models import User
+
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Image(models.Model):
+    id = models.AutoField(primary_key=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='images')
+    image_url = models.ImageField(upload_to='post_images/')
 
 # Create your models here.
