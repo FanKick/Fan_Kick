1c53e189ff3ec4de3e8398e626aeaed70f241804
ryddk <ryddk0301@gmail.com>
2024-07-10 09:53:47 +0900

    feat:base.html

--- delete_migrations.py
+++ delete_migrations.py
@@ -3,19 +3,13 @@ import os
 apps_path = 'apps'
 
 for app in os.listdir(apps_path):
-
     app_migrations_path = os.path.join(apps_path, app, 'migrations')
 
-    if os.path.exists(app_migrations_path):
-        for filename in os.listdir(app_migrations_path):
-            if '_initial.py' in filename:
-                file_path = os.path.join(app_migrations_path, filename)
-                os.remove(file_path)
-                print(f"Deleted: {file_path}")
-
-            elif 'alter' in filename:
-                file_path = os.path.join(app_migrations_path, filename)
-                os.remove(file_path)
-                print(f"Deleted: {file_path}")
-    else:
+    if not os.path.exists(app_migrations_path):
         print(f"No migrations directory found for {app}")
+    else:
+        for filename in os.listdir(app_migrations_path):
+            if '_initial.py' in filename or 'alter' in filename:
+                file_path = os.path.join(app_migrations_path, filename)
+                os.remove(file_path)
+                print(f"Deleted: {file_path}")
