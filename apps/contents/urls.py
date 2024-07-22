aea268021a1c09f3db0f8a30e3d4cc6ab4a305e4
parksungjoon0805 <jjoon9908@naver.com>
2024-07-22 13:46:05 +0900

    Contents

--- apps/contents/urls.py
+++ apps/contents/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('player_post_list/', views.PlayerPostListView.as_view(template_name='contents/player_post_list.html'), name='player_post_list'),
-    path('user_post_list/', views.UserPostListView.as_view(template_name='contents/user_post_list.html'), name='user_post_list'),
-    path('detail/<int:pk>/', views.PostDetailView.as_view(template_name='contents/post_detail.html'), name='post_detail'),
-    path('create/', views.create_post, name='create_post'),
+    path('team/<int:team_id>/player_post_list/', views.PlayerPostListView.as_view(), name='player_post_list'),
+    path('team/<int:team_id>/user_post_list/', views.UserPostListView.as_view(), name='user_post_list'),
+    path('post_detail/<int:post_id>/', views.post_detail_json, name='post_detail_json'),  # JSON 응답을 위한 URL 패턴 추가
+    path('create_post/player/', views.create_post_player, name='create_post_player'),
+    path('create_post/user/', views.create_post_user, name='create_post_user'),
+    path('join_team/', views.join_team, name='join_team'),
 ]
