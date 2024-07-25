a9673f91895c5dab8cd4fa5808c32bf031966371
parksungjoon0805 <jjoon9908@naver.com>
2024-07-25 09:45:55 +0900

    Like & Comment

--- apps/contents/urls.py
+++ apps/contents/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('post_detail/<int:post_id>/', views.post_detail_json, name='post_detail_json'),  # JSON 응답을 위한 URL 패턴 추가
     path('create_post/player/', views.create_post_player, name='create_post_player'),
     path('create_post/user/', views.create_post_user, name='create_post_user'),
-    path('join_team/', views.join_team, name='join_team'),
-     path('post_detail/<int:post_id>/add_comment/', views.add_comment, name='add_comment'),  # add_comment 경로 추가
+    path('team/<int:team_id>/join/', views.join_team, name='join_team'),
+    path('post_detail/<int:post_id>/add_comment/', views.add_comment, name='add_comment'),  # add_comment 경로 추가
+    path('post_detail/<int:post_id>/add_like/', views.add_like, name='add_like'),  # add_like 경로 추가
 ]
