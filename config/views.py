e390746f59c3e21cab8850a2648d705fa79c9eee
ryddk <ryddk0301@gmail.com>
2024-07-27 02:12:02 +0900

    home

--- config/views.py
+++ config/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from apps.accounts.models import Team, Membership
+from apps.info.models import TeamInfo
 
 def home(request):
-    return render(request, 'home.html')
+    user = request.user
+    teams = Team.objects.all()
+    teaminfos = TeamInfo.objects.all()
+
+    if user.is_authenticated:
+        memberships = Membership.objects.filter(user=user, is_active=True).select_related('team')
+    else:
+        memberships = []
+
+    context = {
+        'teams': teams,
+        'teaminfos': teaminfos,
+        'memberships': memberships,
+    }
+
+
+    return render(request, 'home.html', context)
