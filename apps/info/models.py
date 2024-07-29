d8b08df4e082e7886086ed490eddef145ea68ed2
ryddk <ryddk0301@gmail.com>
2024-07-29 15:46:20 +0900

    info models change

--- apps/info/models.py
+++ apps/info/models.py
@@ -1,21 +1,18 @@
-# 1. Id (pk)
-# 2. user_id (fk)
-# 3. team_name
-# 4. banner - team_club_pictures/
-# 5. team_description
-# 6. created_at
-# 7. updated_at
-
+from django.utils.text import slugify
 from django.db import models
 from apps.accounts.models import Team
 
 class TeamInfo(models.Model):
     team = models.OneToOneField(Team, on_delete=models.CASCADE, related_name='team_info')
-    team_name = models.CharField(max_length=255, default='default_team_name')
+    info_team_name = models.CharField(max_length=255, unique=True, blank=True)
     team_club_picture = models.ImageField(upload_to='team_club_pictures/', null=True, blank=True)
-    team_description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.team_name
+    def save(self, args, **kwargs):
+        if not self.info_team_name:
+            self.info_team_name = slugify(self.team.team_name)
+        super().save(args, **kwargs)
+
+    def str(self):
+        return self.team.team_name
