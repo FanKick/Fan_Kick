c369d473e3c0af12ad2321351078bc638079d187
ryddk <ryddk0301@gmail.com>
2024-07-30 10:01:32 +0900

    fix: Finished reservation cancellation feature and fixed bugs

--- apps/info/urls.py
+++ apps/info/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'info'
 
 urlpatterns = [
-    # path('', views.search_view, name='search'),
-    # path('team/<slug:info_team_name>/', views.team_detail, name='team_detail'),
+    path('', views.search_view, name='search'),
+    path('team/<slug:info_team_name>/', views.team_detail, name='team_detail'),
     path('player/<slug:info_player_name>/', views.player_detail, name='player_detail'),
 ]
